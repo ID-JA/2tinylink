969953ad2e87f7a9bdd851fce153536a5cc01b32
ID-JA <idaissa93@gmail.com>
2023-01-22 18:35:27 +0100

    initial create `client` project

---
+++ client/pages/index.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="text-5xl text-center">
+        Let&rsquo;s build a URL shortener!{' '}
+        <span className="text-primary-600">2tinny</span>
+        <span className="font-bold text-danger-600">URL</span> 🚀
+      </div>
+    </div>
+  )
+}
