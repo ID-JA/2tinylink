78d885cb75968b55b81e2624cff809bef69e0d84
AmineTissilguit <amibinary@outlook.com>
2023-02-20 11:32:18 +0100

    Create an extension method for configure the services

--- server/src/WebUI/Extensions/HostingExtensions.cs
+++ server/src/WebUI/Extensions/HostingExtensions.cs
@@ -1,4 +1,14 @@
+using Application;
+using Application.Common.Interfaces.Persistence;
+using Application.Common.Interfaces.Services;
+using Application.CorrespondedUrl.Queries.UrlByAddress;
+using Application.CorrespondedUrl.Queries.UrlByAddress.Behaviors;
+using Application.LinkShortning.Commands.StandardShortening;
+using Application.LinkShortning.Commands.StandardShortening.Behaviors;
+using FluentValidation;
 using Infrastructure.Persistence;
+using Infrastructure.Services;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 using WebUI.Helpers;
 
@@ -6,6 +16,29 @@ namespace WebUI.Extensions
 {
     internal static class HostingExtensions
     {
+        internal static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+        {
+            var configuration = builder.Configuration;
+
+            builder.Services.AddControllers();
+            builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(configuration.GetConnectionString("Default")));
+            builder.Services.AddScoped<IAppDbContext, AppDbContext>();
+            builder.Services.AddSingleton<IUniqueIdProvider, UniqueIdProvider>();
+            builder.Services.AddMediatR(typeof(IApplicationAssemblyReference).Assembly);
+            builder.Services.AddScoped<IPipelineBehavior<StandardShorteningCommand, StandardShorteningResult>, StandardShorteningCommandValidationBehavior>();
+            builder.Services.AddScoped<IPipelineBehavior<UrlByAddressQuery, UrlByAddressQueryResult>, UrlByAddressQueryValidationBehavior>();
+            builder.Services.AddValidatorsFromAssembly(typeof(IApplicationAssemblyReference).Assembly);
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy(Consts.CORS_POLICY_NAME, policy =>
+                {
+                    policy.WithOrigins(configuration["CorsSettings:Origin"])
+                    .AllowAnyMethod();
+                });
+            });
+
+            return builder.Build();
+        }
         internal static WebApplication ConfigurePipeline(this WebApplication app)
         {
             if (app.Environment.IsDevelopment())
--- server/src/WebUI/Program.cs
+++ server/src/WebUI/Program.cs
@@ -1,15 +1,3 @@
-using Application;
-using Application.Common.Interfaces.Persistence;
-using Application.Common.Interfaces.Services;
-using Infrastructure.Persistence;
-using Infrastructure.Services;
-using MediatR;
-using Microsoft.EntityFrameworkCore;
-using FluentValidation;
-using Application.LinkShortning.Commands.StandardShortening;
-using Application.LinkShortning.Commands.StandardShortening.Behaviors;
-using Application.CorrespondedUrl.Queries.UrlByAddress;
-using Application.CorrespondedUrl.Queries.UrlByAddress.Behaviors;
 using WebUI.Extensions;
 
 internal class Program
@@ -17,28 +5,9 @@ internal class Program
     private static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-        var configuration = builder.Configuration;
-
-        // Add services to the container.
-
-        builder.Services.AddControllers();
-        builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(configuration.GetConnectionString("Default")));
-        builder.Services.AddScoped<IAppDbContext, AppDbContext>();
-        builder.Services.AddSingleton<IUniqueIdProvider, UniqueIdProvider>();
-        builder.Services.AddMediatR(typeof(IApplicationAssemblyReference).Assembly);
-        builder.Services.AddScoped<IPipelineBehavior<StandardShorteningCommand, StandardShorteningResult>, StandardShorteningCommandValidationBehavior>();
-        builder.Services.AddScoped<IPipelineBehavior<UrlByAddressQuery, UrlByAddressQueryResult>, UrlByAddressQueryValidationBehavior>();
-        builder.Services.AddValidatorsFromAssembly(typeof(IApplicationAssemblyReference).Assembly);
-        builder.Services.AddCors(options =>
-        {
-            options.AddPolicy("2tinylinkApp", policy =>
-            {
-                policy.WithOrigins(configuration["CorsSettings:Origin"])
-                .AllowAnyMethod();
-            });
-        });
-
-        var app = builder.Build().ConfigurePipeline();
+        
+        var app = builder.ConfigureServices()
+                         .ConfigurePipeline();
 
         app.Run();
     }
