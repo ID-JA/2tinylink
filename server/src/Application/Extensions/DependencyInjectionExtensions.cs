41c13ece997c8838f2b9466bd9bf158a091cc8e4
AmineTissilguit <amibinary@outlook.com>
2023-03-26 18:10:07 +0000

    Simplify service registration for better maintainability

---
+++ server/src/Application/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,32 @@
+using Application.UseCases.Auth.Commands.Register;
+using Application.UseCases.Auth.Commands.Register.Behaviors;
+using Application.UseCases.Auth.Queries.Login;
+using Application.UseCases.Auth.Queries.Login.Behaviors;
+using Application.UseCases.CorrespondedUrl.Queries.UrlByAddress;
+using Application.UseCases.CorrespondedUrl.Queries.UrlByAddress.Behaviors;
+using Application.UseCases.LinkShortening.Commands.StandardShortening;
+using Application.UseCases.LinkShortening.Commands.StandardShortening.Behaviors;
+using Application.UseCases.UserManagement.Queries.UserByUserName;
+using Application.UseCases.UserManagement.Queries.UserByUserName.Behaviors;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Extensions
+{
+    public static class DependencyInjectionExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddMediatR(typeof(IApplicationAssemblyReference).Assembly);
+            services.AddScoped<IPipelineBehavior<StandardShorteningCommand, StandardShorteningResult>, StandardShorteningCommandValidationBehavior>();
+            services.AddScoped<IPipelineBehavior<UrlByAddressQuery, UrlByAddressQueryResult>, UrlByAddressQueryValidationBehavior>();
+            services.AddScoped<IPipelineBehavior<RegisterCommand, RegisterCommandResult>, RegisterCommandValidationBehavior>();
+            services.AddScoped<IPipelineBehavior<UserByUserNameQuery, UserByUserNameQueryResult>, UserByUserNameQueryValidationBehavior>();
+            services.AddScoped<IPipelineBehavior<LoginQuery, LoginQueryResult>, LoginQueryValidationBehavior>();
+            services.AddValidatorsFromAssembly(typeof(IApplicationAssemblyReference).Assembly);
+
+            return services;
+        }
+    }
+}
