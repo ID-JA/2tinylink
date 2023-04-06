37c877c504c7daaeae981ba2c237bf5aa07b27d0
AmineTissilguit <amibinary@outlook.com>
2023-04-06 03:25:59 +0000

    :safety_vest: Add validation to the provided custom alias

--- server/src/Application/UseCases/LinkShortening/Commands/ProShortening/ProShorteningCommandValidator.cs
+++ server/src/Application/UseCases/LinkShortening/Commands/ProShortening/ProShorteningCommandValidator.cs
@@ -1,4 +1,5 @@
 using System.Globalization;
+using Application.Common.Helpers.Consts;
 using Application.UseCases.LinkShortening.Commands.Common;
 using FluentValidation;
 
@@ -8,22 +9,26 @@ namespace Application.UseCases.LinkShortening.Commands.ProShortening
     {
         public ProShorteningCommandValidator()
         {
-            When(x => x.ExpiredAt is not null, () =>
-            {
 
-                RuleFor(x => x.ExpiredAt)
-                .Cascade(CascadeMode.Stop)
-                .Must(x => BeAValidDate(x.ToString())).WithMessage("'{PropertyName}' must be a valid format date")
-                .Must(x => DateTime.Parse(x).ToUniversalTime() > DateTime.Now.AddMinutes(3)).WithMessage("'{PropertyName}' must be in the future.");
+            RuleFor(x => x.ExpiredAt)
+            .Cascade(CascadeMode.Stop)
+            .NotEmpty()
+            .Must(x => BeAValidDate(x.ToString())).WithMessage("'{PropertyName}' must be a valid format date")
+            .Must(x => DateTime.Parse(x).ToUniversalTime() > DateTime.Now.AddMinutes(3)).WithMessage("'{PropertyName}' must be in the future.")
+            .When(x => x.ExpiredAt is not null);
 
-            });
+            RuleFor(x => x.CustomAlias)
+            .Cascade(CascadeMode.Stop)
+            .NotEmpty()
+            .Matches(AppRegEx.ALIAS).WithMessage($"'{{PropertyName}}' must match the pattern '{AppRegEx.ALIAS}'")
+            .When(x => x.CustomAlias is not null);
 
         }
 
         private bool BeAValidDate(string value)
         {
             DateTime date;
-            return DateTime.TryParse(value,CultureInfo.InvariantCulture, out date);
+            return DateTime.TryParse(value, CultureInfo.InvariantCulture, out date);
         }
     }
 }
