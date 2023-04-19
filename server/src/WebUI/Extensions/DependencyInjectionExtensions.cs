fb0100ccd3d7a93198842136193ae5a1cff70f97
ID-JA <idaissa93@gmail.com>
2023-04-19 09:54:21 +0000

    fix(server): update CORS settings

--- server/src/WebUI/Extensions/DependencyInjectionExtensions.cs
+++ server/src/WebUI/Extensions/DependencyInjectionExtensions.cs
@@ -9,17 +9,32 @@ namespace WebUI.Extensions
 
             services.AddControllers();
 
-            services.AddCors(options =>
-            {
-              options.AddPolicy(Consts.CORS_POLICY_NAME, policy =>
-              {
-                  policy.WithOrigins(configuration["CorsSettings:Origin"])
-                        .AllowAnyMethod();
-              });
+            // services.AddCors(options =>
+            // {
+            //   options.AddPolicy(Consts.CORS_POLICY_NAME, policy =>
+            //   {
+            //       policy.WithOrigins(configuration["CorsSettings:Origin"])
+            //             .AllowAnyMethod();
+            //   });
 
-            });
+            // });
+            var corsSettings = configuration.GetSection(nameof(CorsSettings)).Get<CorsSettings>();
+            var origins = new List<string>();
+            if (corsSettings.Origin is not null)
+                origins.AddRange(corsSettings.Origin.Split(';', StringSplitOptions.RemoveEmptyEntries));
+            services.AddCors(opt =>
+                opt.AddPolicy(Consts.CORS_POLICY_NAME, policy =>
+                    policy.AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials()
+                        .WithOrigins(origins.ToArray())));
 
             return services;
         }
+    }
+
+    public class CorsSettings
+    {
+        public string Origin { get; set; }
     }
 }
