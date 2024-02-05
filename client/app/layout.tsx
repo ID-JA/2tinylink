bb6e53f82560bb07aca8aced365bdfe3354f8895
ID-JA <idaissa93@gmail.com>
2024-02-05 20:09:39 +0100

    update dependencies + refactor landing page

---
+++ client/app/layout.tsx
@@ -0,0 +1,27 @@
+import "@mantine/core/styles.css";
+import React from "react";
+import { MantineProvider, ColorSchemeScript } from "@mantine/core";
+import { theme } from "../theme";
+
+export const metadata = {
+  title: "2tinylink app",
+  description: "2tinylink app",
+};
+
+export default function RootLayout({ children }: { children: any }) {
+  return (
+    <html lang="en">
+      <head>
+        <ColorSchemeScript />
+        <link rel="shortcut icon" href="/favicon.svg" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
+        />
+      </head>
+      <body>
+        <MantineProvider theme={theme}>{children}</MantineProvider>
+      </body>
+    </html>
+  );
+}
