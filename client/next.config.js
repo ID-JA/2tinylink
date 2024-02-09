ad936408be885764d89f7afae214e9f5c93c86ac
ID-JA <idaissa93@gmail.com>
2024-02-09 17:00:56 +0100

    remove builder's `HydrationOverlay`

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,3 @@
-const {
-  withHydrationOverlay,
-} = require("@builder.io/react-hydration-overlay/next");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -22,11 +18,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withHydrationOverlay({
-  /**
-   * Optional: `appRootSelector` is the selector for the root element of your app. By default, it is `#__next` which works
-   * for Next.js apps with pages directory. If you are using the app directory, you should change this to `main`.
-   */
-  appRootSelector: "main",
-})(nextConfig);
 module.exports = nextConfig;
