f978150eb20f279f77a04f3adb1b8cd135d64228
ID-JA <idaissa93@gmail.com>
2024-02-09 16:43:28 +0100

    update auth page UI

--- client/next.config.js
+++ client/next.config.js
@@ -1,9 +1,32 @@
+const {
+  withHydrationOverlay,
+} = require("@builder.io/react-hydration-overlay/next");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
     optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "plus.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
 };
 
+module.exports = withHydrationOverlay({
+  /**
+   * Optional: `appRootSelector` is the selector for the root element of your app. By default, it is `#__next` which works
+   * for Next.js apps with pages directory. If you are using the app directory, you should change this to `main`.
+   */
+  appRootSelector: "main",
+})(nextConfig);
 module.exports = nextConfig;
