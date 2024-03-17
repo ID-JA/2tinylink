395e830bb6df3df66bd295135496b640172de684
ID-JA <idaissa93@gmail.com>
2024-03-17 22:59:24 +0000

    refactor login handler

--- server/src/Application/UseCases/Auth/Queries/Login/LoginQueryHandler.cs
+++ server/src/Application/UseCases/Auth/Queries/Login/LoginQueryHandler.cs
@@ -10,19 +10,12 @@ using Microsoft.AspNetCore.Identity;
 
 namespace Application.UseCases.Auth.Queries.Login
 {
-    public class LoginQueryHandler : IRequestHandler<LoginQuery, LoginQueryResult>
+    public class LoginQueryHandler(
+        SignInManager<AppUser> signInManager,
+        UserManager<AppUser> userManager,
+        IJwtProvider jwtProvider)
+        : IRequestHandler<LoginQuery, LoginQueryResult>
     {
-        private readonly SignInManager<AppUser> _signInManager;
-        private readonly UserManager<AppUser> _userManager;
-        private readonly IJwtProvider _jwtProvider;
-
-        public LoginQueryHandler(SignInManager<AppUser> signInManager, UserManager<AppUser> userManager, IJwtProvider jwtProvider)
-        {
-            _signInManager = signInManager;
-            _userManager = userManager;
-            _jwtProvider = jwtProvider;
-        }
-
         public async Task<LoginQueryResult> Handle(LoginQuery query, CancellationToken cancellationToken)
         {
             var isEmail = query.UserNameOrEmail.Contains("@");
@@ -31,28 +24,29 @@ namespace Application.UseCases.Auth.Queries.Login
 
             if (isEmail)
             {
-                user = await _userManager.FindByEmailAsync(query.UserNameOrEmail);
+                user = await userManager.FindByEmailAsync(query.UserNameOrEmail);
             }
             else
             {
-                user = await _userManager.FindByNameAsync(query.UserNameOrEmail);
+                user = await userManager.FindByNameAsync(query.UserNameOrEmail);
             }
 
-            if (user is not null)
-            {
-                SignInResult result = await _signInManager.CheckPasswordSignInAsync(user, query.Password, false);
+            if (user is null)
+                throw new AppException((int)StatusCodes.Status422UnprocessableEntity,
+                    "The username or password you entered is incorrect.");
+            var result = await signInManager.CheckPasswordSignInAsync(user, query.Password, false);
 
-                if (result.Succeeded)
-                {
-                    var token = _jwtProvider.Create(user);
-                    var loginResult = user.Adapt<LoginQueryResult>();
-                    loginResult.Token = token;
-                    return loginResult;
-                }
-                else if (result.IsNotAllowed)
-                {
-                    throw new AppException((int)StatusCodes.Status409Conflict, "The email address is not confirmed.");
-                }
+            if (result.Succeeded)
+            {
+                var token = jwtProvider.Create(user);
+                var loginResult = user.Adapt<LoginQueryResult>();
+                loginResult.Token = token;
+                return loginResult;
+            }
+
+            if (result.IsNotAllowed)
+            {
+                throw new AppException((int)StatusCodes.Status409Conflict, "The email address is not confirmed.");
             }
 
             throw new AppException((int)StatusCodes.Status422UnprocessableEntity, "The username or password you entered is incorrect.");
