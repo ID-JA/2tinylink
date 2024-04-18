71c2854bb8fb8b8031d93ccb77194df76cc0e283
ID-JA <idaissa93@gmail.com>
2024-04-18 16:13:17 +0100

    client: allow users creating new projects

--- client/components/projects/create-project-modal.tsx
+++ client/components/projects/create-project-modal.tsx
@@ -1,10 +1,38 @@
 "use client";
 
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+import { axios } from "@/utils";
 import { Button, Modal, TextInput, Title } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
+import { useMutation } from "@tanstack/react-query";
 
 function CreateProjectModal() {
   const [opened, { open, close }] = useDisclosure(false);
+  const router = useRouter();
+
+  const [data, setData] = useState<{
+    name: string;
+    description: string;
+  }>({
+    name: "",
+    description: "",
+  });
+
+  const { name, description } = data;
+
+  const mutation = useMutation({
+    mutationKey: ["create-project"],
+    mutationFn: async (data: { name: string; description: string }) => {
+      const response = await axios().post("/projects", data);
+      return response.data;
+    },
+    onSuccess: (projectId) => {
+      router.push(`/projects/${projectId}`);
+      close();
+    },
+  });
 
   return (
     <>
@@ -30,10 +58,36 @@ function CreateProjectModal() {
             Create new project
           </Title>
         </div>
-        <form style={{ padding: "2rem 4rem" }}>
-          <TextInput label="Project name" mb="md" />
-          <TextInput label="Description" mb="xl" />
-          <Button fullWidth>Create project</Button>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            mutation.mutate(data);
+          }}
+          style={{ padding: "2rem 4rem" }}
+        >
+          <TextInput
+            required
+            mb="md"
+            name="name"
+            label="Project name"
+            value={name}
+            onChange={(e) => {
+              setData({ ...data, name: e.target.value });
+            }}
+          />
+          <TextInput
+            required
+            mb="xl"
+            name="description"
+            label="Description"
+            value={description}
+            onChange={(e) => {
+              setData({ ...data, description: e.target.value });
+            }}
+          />
+          <Button fullWidth type="submit" loading={mutation.isPending}>
+            Create project
+          </Button>
         </form>
       </Modal>
 
